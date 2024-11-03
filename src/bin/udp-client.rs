4787e5cd839657884892ad80d410752888fe20a7
Wael Karman <wael.karman@gmail.com>
2024-11-03 19:39:15 +0100

    refactor simple client server

--- src/bin/udp-client.rs
+++ src/bin/udp-client.rs
@@ -1,26 +1,47 @@
 use std::net::UdpSocket;
 use std::io::{self, Write};
 
-fn main() -> std::io::Result<()> {
-    // Creiamo una socket UDP
-    let socket = UdpSocket::bind("127.0.0.1:0")?;  // Porta casuale per il client
-    socket.connect("127.0.0.1:8080")?;  // Collegamento al server
 
-    // Lettura dell'input utente
-    print!("Inserisci un messaggio da inviare al server: ");
-    io::stdout().flush()?;  // Flush per mostrare il prompt
-    let mut input = String::new();
-    io::stdin().read_line(&mut input)?;
-
-    // Inviamo il messaggio al server
-    socket.send(input.as_bytes())?;
-
-    // Riceviamo la risposta dal server
-    let mut buffer = [0u8; 1024];
-    let bytes_received = socket.recv(&mut buffer)?;
-    let response = String::from_utf8_lossy(&buffer[..bytes_received]);
-
-    println!("Risposta del server: {}", response);
-
-    Ok(())
+struct UdpClient{
+    socket: UdpSocket,
 }
+
+impl UdpClient {
+
+    fn connect(&mut self) {
+        self.socket.connect("127.0.0.1:8080").unwrap();     
+    }
+
+
+    fn fetch_and_send_loop(&mut self) {
+        print!("Inserisci un messaggio da inviare al server: ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        self.socket.send(input.as_bytes()).unwrap();
+    }
+
+
+    fn allelse(&mut self) -> std::io::Result<()> {
+        let mut buffer = [0u8; 1024];
+        let bytes_received = self.socket.recv(&mut buffer).unwrap();
+        let response = String::from_utf8_lossy(&buffer[..bytes_received]);
+    
+        println!("Risposta del server: {}", response);
+    
+        Ok(())
+    }
+
+}
+
+
+
+fn main() {
+    let mut client = UdpClient{socket: UdpSocket::bind("127.0.0.1:0").unwrap()};
+    client.connect();
+    client.fetch_and_send_loop();
+    client.allelse();
+
+}
+
--- src/bin/udp-server.rs
+++ src/bin/udp-server.rs
@@ -1,23 +1,39 @@
 use std::net::UdpSocket;
 use std::str;
 
-fn main() -> std::io::Result<()> {
-    // Creiamo una socket UDP in ascolto sull'indirizzo specificato
-    let socket = UdpSocket::bind("127.0.0.1:8080")?;
-    println!("Server in ascolto su 127.0.0.1:8080");
+struct UdpServer{
+    socket: UdpSocket,
+}
 
-    let mut buffer = [0u8; 1024];  // Buffer per memorizzare i dati ricevuti
+impl UdpServer {
 
-    loop {
-        // Riceviamo i dati e otteniamo l'indirizzo del client
-        let (bytes_received, client_addr) = socket.recv_from(&mut buffer)?;
-        let msg = str::from_utf8(&buffer[..bytes_received])
-            .expect("Messaggio non in formato UTF-8");
-
-        println!("Ricevuto dal client {}: '{}'", client_addr, msg);
-
-        // Inviamo una risposta al client
-        let response = format!("Messaggio ricevuto: '{}'", msg);
-        socket.send_to(response.as_bytes(), client_addr)?;
+    fn connect(&mut self) { 
+        println!("Server in ascolto su 127.0.0.1:8080");
     }
+
+    fn allelse(&mut self) -> std::io::Result<()> {
+        let mut buffer = [0u8; 1024]; 
+
+        loop {
+            let (bytes_received, client_addr) = self.socket.recv_from(&mut buffer).unwrap();
+            let msg = str::from_utf8(&buffer[..bytes_received]).expect("Messaggio non in formato UTF-8");
+    
+            println!("Ricevuto dal client {}: '{}'", client_addr, msg);
+    
+            let response = format!("Messaggio ricevuto: '{}'", msg);
+            self.socket.send_to(response.as_bytes(), client_addr).unwrap();
+        }
+    }
+
+}
+
+fn main() {
+    let mut server = UdpServer{
+        socket: UdpSocket::bind("127.0.0.1:8080").unwrap()
+    };
+
+    server.connect();
+    server.allelse();
+
+
 }
